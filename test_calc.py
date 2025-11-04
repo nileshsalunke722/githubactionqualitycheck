7a31884c19301ff167bd0f18130db5944235da75
nileshsalunke722 <nileshsalunke722@gmail.com>
2025-11-04 23:43:38 +0530

    test case example

---
+++ test_calc.py
@@ -0,0 +1,16 @@
+import pytest
+
+from calc import add, sub, div, mul
+
+def test_add():
+   assert add(1,3) == 4
+
+def test_mul():
+   assert mul(5,5) == 25
+
+def test_div():
+   assert div(10/5) == 2
+
+def test_sub():
+   assert sub(10,5) == 5
+   
