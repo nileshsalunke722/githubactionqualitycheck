24571614b79101fd6abd0bb8a12b1271bd396705
nileshsalunke722 <nileshsalunke722@gmail.com>
2025-11-04 23:56:13 +0530

    coverage report 4

--- test_calc.py
+++ test_calc.py
@@ -7,8 +7,8 @@ def test_add():
 def test_mul():
    assert mul(5,5) == 25
 
-# def test_div():
-#    assert div(10,5) == 2
+def test_div():
+   assert div(10,5) == 2
 
-# def test_sub():
-#    assert sub(10,5) == 5
+def test_sub():
+   assert sub(10,5) == 5
